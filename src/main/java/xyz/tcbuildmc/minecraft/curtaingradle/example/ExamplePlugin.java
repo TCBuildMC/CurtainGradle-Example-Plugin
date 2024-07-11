eefd89236d3707cac721c09a0408d89b51ecb705
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2024-07-11 22:53:16 +0800

    up

--- src/main/java/xyz/tcbuildmc/minecraft/curtaingradle/example/ExamplePlugin.java
+++ src/main/java/xyz/tcbuildmc/minecraft/curtaingradle/example/ExamplePlugin.java
@@ -1,7 +1,12 @@
 package xyz.tcbuildmc.minecraft.curtaingradle.example;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.tcbuildmc.minecraft.devmetadata.annotation.BukkitPlugin;
 
+@BukkitPlugin(
+        name = "CurtainGradleExamplePlugin",
+        version = "1.0-SNAPSHOT"
+)
 public class ExamplePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
